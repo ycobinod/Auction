27ac4dba533d9801db52d8a6016f1530cf4e4141
binoddhamala <binoddhamla969@gmail.com>
2024-06-26 10:31:07 +0545

    update in model

--- auction/app/models.py
+++ auction/app/models.py
@@ -16,7 +16,7 @@ class Listing(models.Model):
     winner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='winner', null=True, blank=True)
 
     def __str__(self):
-        return f"{self.name} {self.starting_bid}$ {self.owner}"  
+        return f"{self.name} {self.starting_bid} {self.owner}"  
     
 
 class Watchlist(models.Model):
